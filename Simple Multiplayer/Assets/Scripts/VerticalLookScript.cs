5f828439abe9711207cc1c80a766baada73f32d1
downinew <downinew@rose-hulman.edu>
2014-11-09 12:55:34 -0500

    added a new camera for players, will add shooting soon

---
+++ Simple Multiplayer/Assets/Scripts/VerticalLookScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalLookScript : MonoBehaviour {
+
+
+    float mouseSensitivity = 3.0f;
+    float neckRange = 60.0f;
+    float verticalRotation = 0.0f;
+	
+	// Update is called once per frame
+	void Update () {
+
+        float lookY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        verticalRotation -= lookY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -neckRange, neckRange);
+
+        transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+	}
+}
